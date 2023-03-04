9d14e46876c218a238f0322f8009edb6eb0e3bc4
Robero Paes <robertocpaes@gmail.com>
2023-03-03 23:23:41 -0300

    update oauth2 init.sql

--- init.sql
+++ init.sql
@@ -47,7 +47,7 @@ CREATE TABLE IF not  exists  users_oauth_client(
 	oauth_client_id uuid NOT NULL
 );
 
-CREATE TABLE IF not  exists  oauth_client_application(
+CREATE TABLE IF not exists  oauth_client_application(
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     appname text NOT NULL,
     mode INT NOT NULL,
@@ -56,22 +56,48 @@ CREATE TABLE IF not  exists  oauth_client_application(
 	update_at timestamptz default NULL
 );
 
+-- DONT CONTROLLED TABLE THOSE TABLES IS PROVIDED BY GO OAUTH PACKAGE. I THINK I DO THE WRONG CHOOSE. BUT ONLY HAVE MORE ONE WEEK TO CLOSE ALL MY MIND THREADS.
+CREATE TABLE IF not exists oauth2_clients (
+	id text NOT NULL,
+	"secret" text NOT NULL,
+	"domain" text NOT NULL,
+	"data" jsonb NOT NULL,
+	CONSTRAINT oauth2_clients_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF not exists oauth2_tokens (
+	id bigserial NOT NULL,
+	created_at timestamptz NOT NULL,
+	expires_at timestamptz NOT NULL,
+	code text NOT NULL,
+	access text NOT NULL,
+	refresh text NOT NULL,
+	"data" jsonb NOT NULL,
+	CONSTRAINT oauth2_tokens_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_oauth2_tokens_access ON public.oauth2_tokens USING btree (access);
+CREATE INDEX idx_oauth2_tokens_code ON public.oauth2_tokens USING btree (code);
+CREATE INDEX idx_oauth2_tokens_expires_at ON public.oauth2_tokens USING btree (expires_at);
+CREATE INDEX idx_oauth2_tokens_refresh ON public.oauth2_tokens USING btree (refresh);
+
+
 ALTER TABLE users_oauth_client
 ADD FOREIGN KEY (user_id) REFERENCES users (id)
-on delete cascade on update cascade
+ON DELETE CASCADE
 DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE api_keys
 ADD FOREIGN KEY (user_id) REFERENCES users (id)
-on delete cascade on update cascade
+ON DELETE CASCADE
 DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE repos
 ADD FOREIGN KEY (user_id) REFERENCES users (id)
-on delete cascade on update cascade
+ON DELETE CASCADE
 DEFERRABLE INITIALLY DEFERRED;
 
 ALTER TABLE tasks
 ADD FOREIGN KEY (repo_id) REFERENCES repos (id)
-on delete cascade on update cascade
+ON DELETE CASCADE
 DEFERRABLE INITIALLY DEFERRED
